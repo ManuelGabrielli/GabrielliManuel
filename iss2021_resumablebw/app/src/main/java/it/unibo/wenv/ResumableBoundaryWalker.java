0ee3be4ba37f092d7be6a6c15eb8a740a81ff2d6
Manuel Gabrielli <manuel.gabrielli@studio.unibo.it>
2021-03-19 12:22:49 +0100

    Inizio progetto

---
+++ iss2021_resumablebw/app/src/main/java/it/unibo/wenv/ResumableBoundaryWalker.java
@@ -0,0 +1,52 @@
+/*
+===============================================================
+ResumableBoundaryWalker.java
+Use the aril language and the support specified in the
+configuration file IssProtocolConfig.txt
+
+The business logic is defined in RobotControllerArilBoundary
+that is 'message-driven'
+===============================================================
+*/
+package it.unibo.wenv;
+import it.unibo.annotations.ArilRobotSpec;
+import it.unibo.interaction.IssOperations;
+import it.unibo.supports.IssCommSupport;
+import it.unibo.supports.RobotApplicationStarter;
+
+@ArilRobotSpec
+public class ResumableBoundaryWalker {
+    private RobotInputController controller;
+    //private ActorRobotObserver actorObs = new ActorRobotObserver();
+    //Constructor
+    public ResumableBoundaryWalker(IssOperations rs){
+        IssCommSupport rsComm = (IssCommSupport)rs;
+        controller = new RobotInputController(rsComm, true, true );
+        rsComm.registerObserver( controller );
+        //rsComm.registerObserver( actorObs );
+        System.out.println("ResumableBoundaryWalker | CREATED with rsComm=" + rsComm);
+    }
+
+    public String doBoundary(){
+        System.out.println("ResumableBoundaryWalker | doBoundary " + controller  );
+        String result = controller.doBoundary();
+        //actorObs.close();
+        return result;
+    }
+
+    public static void main(String args[]){
+        try {
+            System.out.println("ResumableBoundaryWalker | main start n_Threads=" + Thread.activeCount());
+            //crea istanza classe
+            Object appl = RobotApplicationStarter.createInstance(ResumableBoundaryWalker.class);
+            System.out.println("ResumableBoundaryWalker  | appl n_Threads=" + Thread.activeCount());
+            //esegue il boundary
+            String trip = ((ResumableBoundaryWalker)appl).doBoundary();
+            System.out.println("ResumableBoundaryWalker | trip="   );
+            System.out.println( trip  );
+            System.out.println("ResumableBoundaryWalker | main end n_Threads=" + Thread.activeCount());
+        } catch ( Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
